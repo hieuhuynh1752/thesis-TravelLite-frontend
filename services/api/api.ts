eab1070959e72deeed7cdc53f52ad391175aea2d
Huynh Thai Hieu <thaihieuhuynh1752@gmail.com>
2025-03-13 17:43:14 +0100

    Improve api routing for production 2

--- services/api/api.ts
+++ services/api/api.ts
@@ -3,10 +3,9 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 let API_URL = process.env.NEXT_PUBLIC_SERVER_URL;
-console.log(API_URL);
+
 if (process.env.NODE_ENV === 'production') {
-  API_URL += '/api';
-  console.log(process.env.NEXT_PUBLIC_SERVER_URL);
+  API_URL = 'api';
 }
 const api = axios.create({
   baseURL: API_URL,
